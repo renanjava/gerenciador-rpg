51ee297ac0b59e1e001a70a30f982c31069df403
renanjava <renancvr777@gmail.com>
2025-04-11 20:14:36 -0300

    feat: validando a entrada da classe de personagem e tipo de item magico

--- src/item-magico/dto/create-item-magico.dto.ts
+++ src/item-magico/dto/create-item-magico.dto.ts
@@ -1,8 +1,11 @@
 import { $Enums } from '@prisma/client';
 import { ItemMagicoEntity } from '../entities/item-magico.entity';
+import { IsEnum } from 'class-validator';
 
 export class CreateItemMagicoDto implements ItemMagicoEntity {
   nome: string;
+
+  @IsEnum($Enums.TipoItemMagico, { message: 'Tipo de item mágico inválido' })
   tipoItemMagico: $Enums.TipoItemMagico;
   forca: number;
   defesa: number;
--- src/personagem/dto/create-personagem.dto.ts
+++ src/personagem/dto/create-personagem.dto.ts
@@ -1,9 +1,14 @@
 import { $Enums } from '@prisma/client';
 import { PersonagemEntity } from '../entities/personagem.entity';
+import { IsEnum } from 'class-validator';
 
 export class CreatePersonagemDto implements PersonagemEntity {
   nome: string;
   nomeAventureiro: string;
+
+  @IsEnum($Enums.ClassesPersonagem, {
+    message: 'Personagem contém uma classe inválida',
+  })
   classe: $Enums.ClassesPersonagem;
   level: number;
   forca: number;
