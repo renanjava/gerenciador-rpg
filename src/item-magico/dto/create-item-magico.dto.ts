b3ac9b62dd1caf642b679a6809bb66062cf8d404
renanjava <renancvr777@gmail.com>
2025-04-11 20:30:48 -0300

    feat: garantindo que força e defesa do ItemMagico esteja entre 0-10

--- src/item-magico/dto/create-item-magico.dto.ts
+++ src/item-magico/dto/create-item-magico.dto.ts
@@ -1,13 +1,21 @@
 import { $Enums } from '@prisma/client';
 import { ItemMagicoEntity } from '../entities/item-magico.entity';
-import { IsEnum } from 'class-validator';
+import { IsEnum, Max, Min } from 'class-validator';
 
 export class CreateItemMagicoDto implements ItemMagicoEntity {
   nome: string;
 
   @IsEnum($Enums.TipoItemMagico, { message: 'Tipo de item mágico inválido' })
   tipoItemMagico: $Enums.TipoItemMagico;
+
+  @Min(0, { message: 'A força do item mágico deve ser igual ou maior que 0' })
+  @Max(10, { message: 'A força do item mágico deve ser igual ou menor que 10' })
   forca: number;
+
+  @Min(0, { message: 'A defesa do item mágico deve ser igual ou maior que 0' })
+  @Max(10, {
+    message: 'A defesa do item mágico deve ser igual ou menor que 10',
+  })
   defesa: number;
   personagemId: string;
 }
