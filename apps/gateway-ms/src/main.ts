743cd97216f5d344e597daf5137b8ab2526189a7
Renan <renancvr777@gmail.com>
2025-05-18 00:42:30 -0300

    feat: arquivos de inicialização do nest em todos os serviços

--- apps/gateway-ms/src/main.ts
+++ apps/gateway-ms/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Gerenciador RPG')
+    .setDescription('a descrição do Gerenciador RPG')
+    .setVersion('1.0')
+    .addTag('rpg')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PersonagemModule } from './personagem/personagem.module';
-import { ItemMagicoModule } from './item-magico/item-magico.module';
-import { PrismaModule } from './prisma/prisma.module';
-
-@Module({
-  imports: [PersonagemModule, ItemMagicoModule, PrismaModule],
-})
-export class AppModule {}
